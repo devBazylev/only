c68a44533566145b6f0d93e11b8f2de2d29ce6dc
jar <dev.bazylev@gmail.com>
2025-07-27 16:25:43 +0300

    feat: add ineraction on bullets and btns

--- source/js/modules/info.js
+++ source/js/modules/info.js
@@ -1,6 +1,6 @@
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
-import { debounce } from '../utils/util';
+import { debounce, desk } from '../utils/util';
 
 const initInfo = () => {
   const info = document.querySelector('.info');
@@ -14,11 +14,44 @@ const initInfo = () => {
     const next = frame.querySelector('.info__btn--next');
     const minYear = frame.querySelector('.info__year--min');
     const maxYear = frame.querySelector('.info__year--max');
+    const pag = frame.querySelector('.info__pag');
+    const covers = Array.from(pag.querySelectorAll('.info__cover'));
+    const bullets = pag.querySelectorAll('.info__bullet');
 
-    let i = 0;
     const iMax = sliders.length;
+    let i = 0;
     let min = 1950;
     let max = 1961;
+
+    const setCoord = () => {
+      covers.forEach((cover, i) => {
+        const total = covers.length;
+        const index = (i - 1) % total;
+        const angle = (2 * Math.PI / total) * index;
+        const radius = 13.8;
+        const x = Math.cos(angle) * radius;
+        const y = Math.sin(angle) * radius;
+        cover.style = `left: calc(50% + ${x}vw); top: calc(50% + ${y}vw);`;
+      });
+    };
+
+    const unsetCoord = () => {
+      covers.forEach((cover) => {
+        cover.style = '';
+      });
+    };
+
+    const onDesk = () => {
+      if (desk.matches) {
+        setCoord();
+      } else {
+        unsetCoord();
+      }
+    };
+
+    if (desk.matches) {
+      setCoord();
+    }
 
     const recalcYears = () => {
       const contActive = frame.querySelector(`.info__cont--${i}`);
@@ -68,28 +101,53 @@ const initInfo = () => {
       next.disabled = i === iMax - 1;
     };
 
+    const dataUpdate = () => {
+      frac.textContent = `0${i + 1}`;
+      swapSlider();
+      checkBtnStatus();
+    };
+
     const onPrevDebounced = debounce(() => {
       if (i > 0) {
+        const oldId = i;
         i--;
-        frac.textContent = `0${i + 1}`;
-        swapSlider();
-        checkBtnStatus();
+        activateBullet(oldId);
       }
     });
 
     const onNextDebounced = debounce(() => {
       if (i < iMax - 1) {
+        const oldId = i;
         i++;
-        frac.textContent = `0${i + 1}`;
-        swapSlider();
-        checkBtnStatus();
+        activateBullet(oldId);
       }
     });
 
+    function activateBullet(id, bulletClick = false) {
+      if (id !== i) {
+        covers.forEach(cover => cover.classList.remove('info__cover--active'));
+        if (bulletClick) {
+          i = id;
+        }
+        covers[i].classList.add('info__cover--active');
+        dataUpdate();
+      }
+    }
+
+    const onBullet = (evt) => {
+      const target = evt.target.closest('.info__cover');
+      const id = +target.getAttribute('data-id');
+      activateBullet(id, true);
+    };
+
     checkBtnStatus();
 
+    bullets.forEach((bullet) => {
+      bullet.addEventListener('click', onBullet);
+    });
     prev.addEventListener('click', onPrevDebounced);
     next.addEventListener('click', onNextDebounced);
+    desk.addEventListener('change', onDesk);
 
     sliders.forEach((slider) => {
       const cont = slider.closest('.info__cont');
